6e0664411ff8405ed42ca7c9325c572679559aa5
Nicholas Dove <nick@dovestyles.com>
2020-09-16 21:58:26 -0700

    data- attribute wrappers

--- src/elements.js
+++ src/elements.js
@@ -12,6 +12,36 @@ Element.prototype.findParent = function(selector) {
     return false;
 };
 
+Element.prototype.getData = function(name) {
+    var i, attr, data = {};
+
+    for (i = 0; i < this.attributes.length; i++) {
+        attr = this.attributes[i].name;
+        if (attr.startsWith('data-')) {
+            var key = attr.split('-', 2)[1];
+            if (name) {
+                if (name == key) { return this.attributes[i].value; }
+            } else {
+                data[key] = this.attributes[i].value;
+            }
+        }
+    }
+
+    if (!name) {
+        return data;
+    }
+};
+
+Element.prototype.setData = function(data, val) {
+    if (typeof data == 'object') {
+        for (var key in data) {
+            this.setData(key, data[key]);
+        }
+    } else {
+        this.setAttribute('data-' + data, val);
+    }
+};
+
 export default (function(self) {
     "use strict";
 
